ca78b15f11b3d8b5a61543fd581fe9b4e763775d
Björn Bodensieck <36075498+bbodensieck@users.noreply.github.com>
2026-04-15 07:58:16 +0200

    Update quizData.ts

--- src/data/quizData.ts
+++ src/data/quizData.ts
@@ -8,42 +8,42 @@ export interface Question {
 export const questions: Question[] = [
   {
     id: 1,
-    text: "Station LFZ: Wie heißt die Meldende aus dem Landesmuseum mit Namen?",
+    text: "Wie heißt die Meldende aus dem Landesmuseum mit Namen?",
     correctAnswer: "Jansen",
   },
   {
     id: 2,
-    text: "Station Spurensicherung: Von welcher Marke war die kleinere Schuhabdruckspur?",
+    text: "Von welcher Marke war die kleinere Schuhabdruckspur?",
     correctAnswer: "Adidas",
   },
   {
     id: 3,
-    text: "Station Videoauswertung: Ist die Täterin Linkshänderin oder Rechtshänderin?",
+    text: "Ist die Täterin Linkshänderin oder Rechtshänderin?",
     correctAnswer: "Rechtshänderin",
   },
   {
     id: 4,
-    text: "Station Verwaltung: Wie lautet der Name der Putzfirma?",
+    text: "Wie lautet der Name der Putzfirma?",
     correctAnswer: "Glanzwerk",
   },
   {
     id: 5,
-    text: "Station Zeugenvernehmung: Wie ist der Spitzname der Täterin?",
+    text: "Wie ist der Spitzname der Täterin?",
     correctAnswer: "Tina",
   },
   {
     id: 6,
-    text: "Station Fahrzeuge/Ausrüstung: Welches ist das wichtigste Einsatzmittel für einen Polizeibeamten im Streifendienst?",
+    text: "Welches ist das wichtigste Einsatzmittel für einen Polizeibeamten im Streifendienst?",
     correctAnswer: "Funkgerät",
   },
   {
     id: 7,
-    text: "Station aktive Fragerunde: Wer ist von der Polizei in der Regel zuerst am Einsatzort? (Abkürzung)",
+    text: "Wer ist von der Polizei in der Regel zuerst am Einsatzort? (Abkürzung)",
     correctAnswer: "ESD",
   },
   {
     id: 8,
-    text: "Station PG: Welches Tier zeigt das Tattoo des männlichen Täters?",
+    text: "Welches Tier zeigt das Tattoo des männlichen Täters?",
     correctAnswer: "Schlange",
   },
 ];
